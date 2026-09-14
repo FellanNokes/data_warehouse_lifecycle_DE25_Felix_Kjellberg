091498f39c79a06ece7f1e2d23e8e10cc9bccae3
FellanNokes <felix.l.kjellberg@gmail.com>
2026-09-14 13:19:30 +0200

    setup ice cream db and reader and writer role

---
+++ 05_user_and_roles/setup_dwh.sql
@@ -0,0 +1,11 @@
+USE ROLE SYSADMIN;
+
+CREATE WAREHOUSE dev_wh
+WITH
+    WAREHOUSE_SIZE = 'X-Small'
+    AUTO_SUSPEND = 60
+    AUTO_RESUME = TRUE
+    INITIALLY_SUSPENDED = TRUE
+    COMMENT = "Warehouse for development and analysis";
+
+SHOW WAREHOUSES;
