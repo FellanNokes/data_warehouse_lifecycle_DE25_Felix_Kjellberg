e9a7f8afb9287f92c2d31aa1c1df4e2233fbd231
FellanNokes <felix.l.kjellberg@gmail.com>
2026-09-15 10:49:29 +0200

    added users and roles

---
+++ 06_dlt_extract_load_csv_dlt/worksheet_snowflake/setup_user_role.sql
@@ -0,0 +1,29 @@
+USE ROLE USERADMIN;
+
+CREATE ROLE IF NOT EXISTS movies_dlt_role;
+CREATE ROLE IF NOT EXISTS movies_reader_role;
+CREATE USER IF NOT EXISTS extract_loader
+    PASSWORD = ''
+    DEFAULT_WAREHOUSE = dev_wh;
+
+USE ROLE SECURITYADMIN;
+
+-- grant to users
+GRANT ROLE movies_dlt_role TO USER extract_loader;
+
+GRANT ROLE movies_reader_role TO USER nokes;
+
+-- grant privileges to role
+GRANT USAGE ON WAREHOUSE dev_wh TO ROLE movies_dlt_role;
+GRANT USAGE ON DATABASE movies TO ROLE movies_dlt_role;
+GRANT USAGE ON SCHEMA movies.staging TO ROLE movies_dlt_role;
+GRANT CREATE TABLE ON SCHEMA movies.staging TO ROLE movies_dlt_role;
+GRANT INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA movies.staging TO ROLE movies_dlt_role;
+GRANT INSERT, UPDATE, DELETE ON FUTURE TABLES IN SCHEMA movies.staging TO ROLE movies_dlt_role;
+
+
+-- check grants
+SHOW GRANTS ON SCHEMA movies.staging;
+SHOW FUTURE GRANTS IN SCHEMA movies.staging;
+SHOW GRANTS TO ROLE movies_dlt_role;
+SHOW GRANTS TO USER extract_loader;
